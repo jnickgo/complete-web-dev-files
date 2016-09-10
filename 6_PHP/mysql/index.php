646f9b2bc9f164ba85d8cd36940b7aea0c90409e
Nick <nickgover@gmail.com>
2016-09-09 22:33:45 -0500

    started session variables.

---
+++ 6_PHP/mysql/index.php
@@ -0,0 +1,55 @@
+<?php
+
+    session_start();
+
+    // create a session variable
+    $_SESSION['loginid']=1;
+
+    echo $_SESSION['loginid'];
+
+
+
+    // create a database connection.
+    $hostname = "dev.learning.nickgover.com";
+    $username = "justauser";
+    $password = "soUiu0dW";
+    $database = "phpcourse";
+
+    $dbLink = mysqli_connect("dev.learning.nickgover.com", "justauser", "soUiu0dW", "phpcourse");
+
+    if (mysqli_connect_error()) {
+        
+        // Kill the application.
+        die("Couldn't connect to database.");
+    }
+
+    // Column variables
+    $id = 4;
+    $pokedexId = "004";
+    $name = "Charmander";
+    $types = "Fire";
+
+    // insert statement
+    //$queryInsertStatement = "INSERT INTO `pokemon` (`id`, `pokedex_id`, `name` `types`) VALUES('$id', '$pokedexId', '$name', '$types')";
+    //  mysqli_query($dbLink, $queryInsertStatement);
+        
+    // update statement
+    //$queryUpdateStatement = "UPDATE `pokemon` SET `types`='$types' WHERE `id`=3 LIMIT 1";
+    //mysqli_query($dbLink, $queryUpdateStatement);
+
+    // select statement
+    $querySelectStatement = "SELECT * FROM pokemon";
+
+    if ($resultSet = mysqli_query($dbLink, $querySelectStatement)) {
+
+        // print out each row
+        while ($row = mysqli_fetch_array($resultSet)) {
+            print_r($row);
+        }
+
+    } else {
+
+        echo "Query failed.";
+    }
+    
+?>
