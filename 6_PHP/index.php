c082984c8c609bc1be21315133677cdf7d4fa3de
Nick <nickgover@gmail.com>
2016-10-01 08:19:35 -0500

    Diary-project

--- 6_PHP/index.php
+++ 6_PHP/index.php
@@ -5,7 +5,7 @@
 
 <head>
 
-    <title>PHP Login Form</title>
+    
     <title>My Little Diary</title>
     <meta charset="utf-8" />
     <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
@@ -15,33 +15,113 @@
         crossorigin="anonymous">
 
 
+    <!-- Styling -->
+    <style>    
+        .navbar-brand {
+            font-size: 1.8em;
+        }
+        
+        #topContainer {
+            background-image: url("http://www.pixelstalk.net/wp-content/uploads/2016/05/Silver-Blur-Background-Wallpaper.jpg");
+            height: 400px;
+            width: 100%;
+            background-size: cover;
+        }
+        
+        #topRow {
+            margin-top: 100px;
+            text-align: center;
+        }
+        .bold {
+            font-weight: bold;
+        }
+        .marginTop {
+            margin-top: 25px;
+        }
+        
+        .center {
+            text-align: center;
+        }
+        
+        .title {
+            margin-top: 100px;
+            font-size: 300%;
+        }
+        
+        #footer {
+            background-color: #B0D1FB;
+            padding-top: 70px;
+            width: 100%;
+        }
+        
+        .marginBottom {
+            margin-bottom: 30px;
+        }
+        
+        .playStoreImage {
+            width: 250px;
+        }
+    </style>
 </head>
 
     <body>
-        <!-- Sign-up form -->
-        <form method="post">
-            <!-- reload email on failed login -->
-            <input type="email" name="email" id="email" value="<?php echo addslashes($_POST['email']); ?>" />
 
-            <!-- reload password on failed login -->
-            <input type="password" name="password" value="<?php echo addslashes($_POST['password']); ?>" />
+    <div class="navbar navbar-default navbar-fixed-top">
 
-            <input type="submit" name="submit" value="Sign Up" />
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">     
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand">My App</a>
+            </div>
 
-        </form>
+            <div class="collapse navbar-collapse">
 
-        <!-- Log-in form -->
-        <form method="post">
-            <!-- reload email on failed login -->
-            <input type="email" name="loginEmail" id="loginEmail" value="<?php echo addslashes($_POST['email']); ?>" />
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#home">Home</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
 
-            <!-- reload password on failed login -->
-            <input type="password" name="loginPassword" value="<?php echo addslashes($_POST['password']); ?>" />
 
-            <input type="submit" name="submit" value="Log In" />
+        <div class="container contentContainer" id="topContainer">
+            <div class="row">
+                
+                <div class="col-md-6 col-md-offset-3" id="topRow">
+                    
+                    <h1>My Awesome App</h1>
+                    
+                    <!-- Sign-up form -->
+                    <form method="post">
+                        <!-- reload email on failed login -->
+                        <input type="email" name="email" id="email" value="<?php echo addslashes($_POST['email']); ?>" />
 
-        </form>
+                        <!-- reload password on failed login -->
+                        <input type="password" name="password" value="<?php echo addslashes($_POST['password']); ?>" />
 
+                        <input type="submit" name="submit" value="Sign Up" />
+
+                    </form>
+
+                    <!-- Log-in form -->
+                    <form method="post">
+                        <!-- reload email on failed login -->
+                        <input type="email" name="loginEmail" id="loginEmail" value="<?php echo addslashes($_POST['email']); ?>" />
+
+                        <!-- reload password on failed login -->
+                        <input type="password" name="loginPassword" value="<?php echo addslashes($_POST['password']); ?>" />
+
+                        <input type="submit" name="submit" value="Log In" />
+
+                    </form>
+
+                </div>
+            </div>
+        </div>
 
         <!-- Latest compiled and minified JavaScript -->
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
